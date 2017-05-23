4cdfbe0ac9a3b985961704282f7d4c012b5080f4
berenisser <berenisser@gmail.com>
2017-05-23 19:59:16 -0400

    Validation for input length for X and Y done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//--------------------Validacion de boton Become a Driver---------------------------------
+
 document.getElementById("btn-driver").addEventListener("click",function(){
 	var inputNumber = document.getElementById("input-number");
 	event.preventDefault(); //previene que la página salte al inicio
@@ -16,8 +18,56 @@ function validarFono(inputNumber){
 		return true;
 	}else{
 		inputNumber.value = "";
-		alert("Debe ingresar un número de teléfono válido");  
+		alert("Debe ingresar un número de teléfono válido con más de 9 dígitos");  
 		inputNumber.focus();  
 		return false; 
 	}
 }
+
+//-------------------Seccion del tablero de juego-----------------------------------
+
+//Extraigo los input coordenada
+var x_coor = document.getElementById("x-coor");
+var y_coor = document.getElementById("y-coor");
+var x = x_coor.value -1;
+var y = y_coor.value -1;
+
+//------------Desaparezco la caja con la petición de coordenadas y verifico-------------------------------------
+
+document.getElementById("btn-start").addEventListener("click",function(){
+	var x_coor = document.getElementById("x-coor");
+	var y_coor = document.getElementById("y-coor");
+	event.preventDefault(); //previene que la página salte al inicio
+	
+	if(validarX(x_coor)){
+		if(validarY(y_coor)){
+			return true;
+		}
+	}
+});
+
+function validarX(x_coor){
+	var numero = /^[0-9]$/;
+	if(x_coor.value.match(numero) || x_coor.value <= 10){
+		return true;
+	}else{
+		x_coor.value = "";
+		alert("Debe ingresar un número entre 1 y 10 para la coordenada X");  
+		x_coor.focus();  
+		return false; 
+	}
+}
+
+function validarY(y_coor){
+	var numero = /^[0-9]$/;
+	if(y_coor.value.match(numero) && y_coor.value <= 6){
+		return true;
+	}else{
+		y_coor.value = "";
+		alert("Debe ingresar un número entre 1 y 6 para la coordenada Y");  
+		y_coor.focus();  
+		return false; 
+	}
+}
+
+//------------------------------------------------------------------------
